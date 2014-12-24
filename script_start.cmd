26d38ef0feaef57e69b5eafb25f69c53eff7c0d8
Philippe Sawicki <philsawicki@users.noreply.github.com>
2014-12-24 01:38:43 -0500

    Adds unit tests for StockListController
    Adds some unit tests for StockListController, validating that the
    default watched stocks are set.

--- script_start.cmd
+++ script_start.cmd
@@ -1,4 +1,10 @@
+REM Runs automated tasks commonly used during development, such as:
+REM   * Starting HTTP server.
+REM   * Gulp Watch-ing file changes to build /dist project files.
+REM   * Karma Watch-ing file changes to run automated unit tests.
+
 start cmd /k "npm start"
 start cmd /k "gulp watch"
 start cmd /k "npm test"
+
 exit
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -1,3 +1,9 @@
+'use strict';
+
+/**
+ * Unit Tests for Controllers.
+ */
+
 describe('HomePageController', function() {
 	beforeEach(module('myApp'));
 
@@ -22,15 +28,68 @@ describe('AboutPageController', function() {
 });
 
 
-/*
+
+
 describe('StockListController', function() {
+	var $httpBackend, $rootScope, createController;
+
+	var constants = {
+		DefaultJSONPRequest: 'http://query.yahooapis.com/v1/public/yql?q=SELECT%20*%20FROM%20csv%20WHERE%20url%3D%22http%3A%2F%2Fwww.google.com%2Ffinance%2Fgetprices%3Fq%3DT%26x%3DTSE%26i%3D900%26p%3D1d%26f%3Dd%2Cc%2Cv%2Ck%2Co%2Ch%2Cl%26df%3Dcpct%26auto%3D0%26ei%3DEf6XUYDfCqSTiAKEMg%22&format=json&callback=JSON_CALLBACK'
+	};
+
+
+
+	// Set up the module:
 	beforeEach(module('myApp'));
 
-	it('should display an introduction message with controller version', inject(function($controller) {
-		var scope = {},
-		    controller = $controller('StockListController', {$scope:scope});
+	beforeEach(inject(function($injector) {
+		// Set up the mock http service responses:
+		$httpBackend = $injector.get('$httpBackend');
+		$httpBackend.whenJSONP(constants.DefaultJSONPRequest)
+			.respond(
+				{} // TODO: Complete this with mock response data.
+			);
+		
+		// Get hold of a scope (i.e. the root scope):
+		$rootScope = $injector.get('$rootScope');
+		
+		// The $controller service is used to create instances of controllers:
+		var $controller = $injector.get('$controller');
 
-		expect(scope.stockQuotes.length).toBe(10);
+		createController = function() {
+			return $controller('StockListController', {
+				'$scope': $rootScope
+			});
+		};
 	}));
+
+
+
+	afterEach(function() {
+		//$httpBackend.verifyNoOutstandingExpectation();
+		//$httpBackend.verifyNoOutstandingRequest();
+	});
+
+
+
+	it('should have a default "Refresh Interval"', function() {
+		var controller = createController();
+
+		expect($rootScope.refreshInterval).toBeDefined();
+		expect($rootScope.refreshInterval).toEqual(30);
+	});
+
+	it('should have default Portfolio entries', function() {
+		var controller = createController();
+
+		expect($rootScope.stockQuotes).toBeDefined();
+		expect($rootScope.stockQuotes.length).toBe(10);
+	});
+
+	//it('should fetch things', function() {
+	//	$httpBackend.expectJSONP(constants.DefaultJSONPRequest);
+	//
+	//	var controller = createController();
+	//	//$httpBackend.flush();
+	//});
 });
-*/
