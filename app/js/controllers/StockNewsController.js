864a2df9da8cb2844137c57c40f11ae7f87c4a65
Philippe Sawicki <philsawicki@users.noreply.github.com>
2015-01-16 22:57:05 -0500

    Optimizes performance of ng-Repeats
    Prevents DOM from being edited too often by adding "track by [...]" for
    Stock List and News List Views items.

--- app/js/controllers/StockNewsController.js
+++ app/js/controllers/StockNewsController.js
@@ -13,23 +13,12 @@ angular.module('stockWatcher.Controllers')
 		// Set the initial list of news items:
 		$scope.newsItems = [];
 
-		// Timestamp of the last news item:
-		var lastRefreshTimestamp = 0;
-
 
 		var getNewsForStock = function() {
 			var promise = stockService.getNewsFeedForStock($scope.symbol);
 			promise.then(
 				function (data) {
-					for (var i = data.length - 1; i >= 0; i--) {
-						var newsItem = data[i];
-
-						var newsItemPublishDate = new Date(newsItem.pubDate);
-						if (newsItemPublishDate > lastRefreshTimestamp) {
-							$scope.newsItems.push(newsItem);
-							lastRefreshTimestamp = newsItemPublishDate;
-						}
-					}
+					$scope.newsItems = data;
 				},
 				function (reason) {
 					if (reason) {
